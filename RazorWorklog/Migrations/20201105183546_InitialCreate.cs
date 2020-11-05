d544033cecc7e4c6844a15f19e1190fc7f8066bb
Rahul Pon <rahwinside@gmail.com>
2020-11-06 00:25:06 +0530

    First commit - CRUD added
     Author:    Rahul Pon <rahwinside@gmail.com>

---
+++ RazorWorklog/Migrations/20201105183546_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorWorklog.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Work",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(nullable: true),
+                    WorkDate = table.Column<DateTime>(nullable: false),
+                    Hours = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Work", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Work");
+        }
+    }
+}
---
+++ RazorWorklog/Models/Work.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorWorklog.Models
+{
+    public class Work
+    {
+
+        public int ID { get; set; }
+        public string Description { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime WorkDate { get; set; }
+        public decimal Hours { get; set; }
+
+    }
+}
